05fdb598820e6ece415017de4df8571eeba45838
NobuyukiMorii <qwerty.poiu.mory@gmail.com>
2015-05-24 17:27:52 +0800

    googleログインの処理はつけた。

--- SampleTableView002/UploadViewController.m
+++ SampleTableView002/UploadViewController.m
@@ -7,33 +7,27 @@
 //
 
 #import "UploadViewController.h"
+#import "GTMOAuth2Authentication.h"
+#import "GTMOAuth2ViewControllerTouch.h"
 
 @interface UploadViewController ()
 
+@property (nonatomic, retain) GTMOAuth2Authentication *auth;
+-(void) startLogin;
+
 @end
+
+static NSString *const kKeychainItemName = @"GOAuthTest";
+static NSString *const scope = @"https://www.googleapis.com/auth/calendar";// Calendar APIを利用する場合のscope
+static NSString *const clientId = @"399027015882-d92fksp0gn87tjeudelrhirqgkkae9bi.apps.googleusercontent.com";// 発行されたClient IDを設定
+static NSString *const clientSecret = @"HPL-NJzOtVVQjxg9lVy4jmtz";// 発行されたClient Secretを設定
+static NSString *const hasLoggedIn = @"hasLoggedInKey";// NSUserDefaultに保存するための文字列
+
 
 @implementation UploadViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    //画面の大きさを取得する
-    UIWebView *wv = [[UIWebView alloc] init];
-    wv.delegate = self;
-    
-    //ステータスバーの高さ
-    float statusHeight = [[UIApplication sharedApplication] statusBarFrame].size.height;
-    
-    wv.frame = CGRectMake(0, self.navigationController.navigationBar.bounds.size.height+statusHeight, self.view.bounds.size.width, self.view.bounds.size.height-self.navigationController.navigationBar.bounds.size.height-statusHeight);
-    wv.scalesPageToFit = YES;
-    [self.view addSubview:wv];
-    
-    NSLog(@"%@",_RestId);
-    NSString *str = @"http://localhost:8888/GourRepoM2/Movies/selectRestForAddMovie";
-    NSURL *url = [NSURL URLWithString:str];
-    NSURLRequest *req = [NSURLRequest requestWithURL:url];
-    [wv loadRequest:req];
-
     
 }
 
@@ -70,5 +64,99 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // アプリ起動してOAuth認証動作を開始する
+    [self startLogin];
+}
+
+// OAuth認証の開始
+- (void)startLogin
+{
+    // 既に認証をしたかどうか確認
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    BOOL hasLoggedin = [defaults boolForKey:hasLoggedIn];
+    
+    if(hasLoggedin == YES) {
+        // 認証したことがある場合
+        self.auth = [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:kKeychainItemName
+                                                                          clientID:clientId
+                                                                      clientSecret:clientSecret];
+        // アクセストークンの取得
+        [self authorizeRequest];
+        
+        //WebViewの表示
+        [self displayWebView];
+    } else {
+        // 認証したことがない場合
+        GTMOAuth2ViewControllerTouch *gvc = [[GTMOAuth2ViewControllerTouch alloc] initWithScope:scope
+                                                                                       clientID:clientId
+                                                                                   clientSecret:clientSecret
+                                                                               keychainItemName:kKeychainItemName
+                                                                                       delegate:self
+                                                                               finishedSelector:@selector(viewController:finishedWithAuth:error:)
+                                             ];
+        // 認証画面の表示
+        [self presentViewController:gvc animated:YES completion:nil];
+    }
+}
+
+// 認証後に実行する処理
+- (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController
+      finishedWithAuth:(GTMOAuth2Authentication *)auth
+                 error:(NSError *)error
+{
+    if(error != nil) {
+        // 認証失敗
+    } else {
+        // 認証成功
+        self.auth = auth;
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        [defaults setBool:YES forKey:hasLoggedIn];
+        [defaults synchronize];
+        
+        // アクセストークンの取得
+        [self authorizeRequest];
+        
+        //WebViewの表示
+        [self displayWebView];
+ 
+        
+    }
+    
+    // 認証画面を閉じる
+    [viewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+// アクセストークンの取得処理
+- (void)authorizeRequest
+{
+    NSLog(@"%@", self.auth);
+    NSMutableURLRequest *req = [[NSMutableURLRequest alloc] initWithURL:self.auth.tokenURL];
+    [self.auth authorizeRequest:req completionHandler:^(NSError *error) {
+        NSLog(@"%@", self.auth);
+    }];
+}
+
+//WebViewの表示
+- (void)displayWebView{
+    //画面の大きさを取得する
+    UIWebView *wv = [[UIWebView alloc] init];
+    wv.delegate = self;
+    
+    //ステータスバーの高さ
+    float statusHeight = [[UIApplication sharedApplication] statusBarFrame].size.height;
+    
+    wv.frame = CGRectMake(0, self.navigationController.navigationBar.bounds.size.height+statusHeight, self.view.bounds.size.width, self.view.bounds.size.height-self.navigationController.navigationBar.bounds.size.height-statusHeight);
+    wv.scalesPageToFit = YES;
+    [self.view addSubview:wv];
+    
+    NSLog(@"%@",_RestId);
+    NSString *str = @"http://localhost:8888/GourRepoM2/Movies/selectRestForAddMovie";
+    NSURL *url = [NSURL URLWithString:str];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    [wv loadRequest:req];
+}
 
 @end
